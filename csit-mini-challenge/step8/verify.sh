1ea5b30fa92bc741aa60577b485b295e41d40f63
Chua Song Ann <chuasongann@gmail.com>
2024-10-08 23:05:50 +0800

    Update verify.sh

--- csit-mini-challenge/step8/verify.sh
+++ csit-mini-challenge/step8/verify.sh
@@ -7,4 +7,5 @@ analysisPort=$(kubectl get pod -n default analysis-pod -o jsonpath='{.spec.conta
 kubectl get svc -n default investigation-svc
 kubectl get svc -n default analysis-svc
 
-if [[ [[ "${running}" != "Running" ]] ]] || [[ "${imageName}" != "sachua/task-3:v0.0.1" ]] || [[ "${investigationPort}" != "80" ]] || [[ "${analysisPort}" != "80" ]]; then exit 1; fi
+if [[ "${running}" != "Running" ]] || [[ "${running}" != "Completed" ]];then exit 1; fi
+if [[ "${imageName}" != "sachua/task-3:v0.0.1" ]] || [[ "${investigationPort}" != "80" ]] || [[ "${analysisPort}" != "80" ]]; then exit 1; fi
