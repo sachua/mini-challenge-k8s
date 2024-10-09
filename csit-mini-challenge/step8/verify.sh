62446940d3d9802778e049a0a356cf82df8e85db
Chua Song Ann <chuasongann@gmail.com>
2024-10-09 13:58:13 +0800

    Update verify.sh

--- csit-mini-challenge/step8/verify.sh
+++ csit-mini-challenge/step8/verify.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-running=$(kubectl get deploy -n default command-center -o jsonpath='{.status.readyReplicas}')
-imageName=$(kubectl get deploy -n default command-center -o jsonpath='{.spec.template.spec.containers[0].image}')
-investigationPort=$(kubectl get deploy -n default investigation-unit -o jsonpath='{.spec.template.spec.containers[0].ports[?(@.containerPort==80)].containerPort}')
-analysisPort=$(kubectl get deploy -n default analysis-unit -o jsonpath='{.spec.template.spec.containers[0].ports[?(@.containerPort==80)].containerPort}')
-kubectl get svc -n default investigation-unit
-kubectl get svc -n default analysis-unit
+running=$(kubectl get deployment -n default command-center -o jsonpath='{.status.readyReplicas}')
+imageName=$(kubectl get deployment -n default command-center -o jsonpath='{.spec.template.spec.containers[0].image}')
+investigationPort=$(kubectl get deployment -n default investigation-unit -o jsonpath='{.spec.template.spec.containers[0].ports[?(@.containerPort==80)].containerPort}')
+analysisPort=$(kubectl get deployment -n default analysis-unit -o jsonpath='{.spec.template.spec.containers[0].ports[?(@.containerPort==80)].containerPort}')
+kubectl get service -n default investigation-unit
+kubectl get service -n default analysis-unit
 
 if [[ "${running}" < 1 ]] || [[ "${imageName}" != "sachua/task-3:v0.0.1" ]] || [[ "${investigationPort}" != "80" ]] || [[ "${analysisPort}" != "80" ]]; then exit 1; fi
