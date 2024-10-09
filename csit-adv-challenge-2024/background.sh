4b8e61a3ba8032a1d3ee13229618f930b0d0494b
sachua <chuasongann@gmail.com>
2024-10-09 15:58:55 +0800

    update background.sh

--- csit-adv-challenge-2024/background.sh
+++ csit-adv-challenge-2024/background.sh
@@ -2,25 +2,25 @@
 
 ssh node01 systemctl stop kubelet
 
-kubectl apply -f - <<EOF \
-apiVersion: apps/v1 \
-kind: Deployment \
-metadata: \
-  name: analysis-unit \
-spec: \
-  selector: \
-    matchLabels: \
-      app: analysis-unit \
-  template: \
-    metadata: \
-      labels: \
-        app: analysis-unit \
-    spec: \
-      nodeSelector: \
-        kubernetes.io/hostname: node01 \
-      containers: \
-      - name: task-2 \
-        image: sachua/task-2:v0.0.1 \
-        ports: \
-        - containerPort: 80 \
+kubectl apply -f - <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: analysis-unit
+spec:
+  selector:
+    matchLabels:
+      app: analysis-unit
+  template:
+    metadata:
+      labels:
+        app: analysis-unit
+    spec:
+      nodeSelector:
+        kubernetes.io/hostname: node01
+      containers:
+      - name: task-2
+        image: sachua/task-2:v0.0.1
+        ports:
+        - containerPort: 80
 EOF
