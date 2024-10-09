34bda2bb05d1e30c8a125eaa0811ceb11b0a6047
Chua Song Ann <chuasongann@gmail.com>
2024-10-09 11:04:58 +0800

    Update verify.sh

--- csit-mini-challenge/step5/verify.sh
+++ csit-mini-challenge/step5/verify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 imageName=$(kubectl get deploy -n default analysis-unit -o jsonpath='{.spec.template.spec.containers[0].image}')
-location=$(kubectl exec -n default -l app=analysis-unit -- cat /mnt/location/tipoff)
+location=$(kubectl exec -n default deploy/analysis-unit -- cat /mnt/location/tipoff)
 if [[ "${imageName}" != "sachua/task-2:v0.0.1" ]] || [[ "${location}" != "gAAAAABnBNfVYBZhvKfjHG4DCGSMmWshfu6VYtiERpvz3JiHn8ob0lovnqSqbv_gCKQDSW1uG7NpVfIEJSktUvTZk-dXxKn28RXCK_d1cGAkquq2CEYg8NI=" ]]; then exit 1; fi
