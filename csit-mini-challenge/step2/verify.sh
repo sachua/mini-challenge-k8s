d73c6c07caa08f6c4692fc9c355f0bcd99d33795
Chua Song Ann <chuasongann@gmail.com>
2024-10-09 10:56:54 +0800

    Update verify.sh

--- csit-mini-challenge/step2/verify.sh
+++ csit-mini-challenge/step2/verify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-running=$(kubectl get pod -n default -l=app=investigation-unit -o jsonpath='{.items[0].status.phase}')
+running=$(kubectl get pod -n default -l app=investigation-unit -o jsonpath='{.items[0].status.phase}')
 imageName=$(kubectl get deploy -n default investigation-unit -o jsonpath='{.spec.template.spec.containers[0].image}')
 if [[ "${imageName}" != "sachua/task-1:v0.0.1" ]] || [[ "${running}" != "Running" ]]; then exit 1; fi
