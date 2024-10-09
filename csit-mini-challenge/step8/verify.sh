b24462de3027681703a2ae215b76314565ab9299
Chua Song Ann <chuasongann@gmail.com>
2024-10-09 11:13:24 +0800

    Update verify.sh

--- csit-mini-challenge/step8/verify.sh
+++ csit-mini-challenge/step8/verify.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-running=$(kubectl get pod -n default investigation-pod -o jsonpath='{.status.phase}')
-imageName=$(kubectl get pod -n default command-center -o jsonpath='{.spec.containers[?(@.name=="command-center")].image}')
-investigationPort=$(kubectl get pod -n default investigation-pod -o jsonpath='{.spec.containers[?(@.name=="investigation-pod")].ports[?(@.containerPort==80)].containerPort}')
-analysisPort=$(kubectl get pod -n default analysis-pod -o jsonpath='{.spec.containers[?(@.name=="analysis-pod")].ports[?(@.containerPort==80)].containerPort}')
-kubectl get svc -n default investigation-svc
-kubectl get svc -n default analysis-svc
+running=$(kubectl get pod -n default -l app=command-center -o jsonpath='{.items[0].status.phase}')
+imageName=$(kubectl get deploy -n default command-center -o jsonpath='{.spec.template.spec.containers[0].image}')
+investigationPort=$(kubectl get pod -n default -l app=investigation-unit -o jsonpath='{.items[0].spec.containers[0].ports[?(@.containerPort==80)].containerPort}')
+analysisPort=$(kubectl get pod -n default -l app=analysis-unit -o jsonpath='{.items[0].spec.containers[0].ports[?(@.containerPort==80)].containerPort}')
+kubectl get svc -n default investigation-unit
+kubectl get svc -n default analysis-unit
 
 if [[ "${running}" != "Running" ]] || [[ "${imageName}" != "sachua/task-3:v0.0.1" ]] || [[ "${investigationPort}" != "80" ]] || [[ "${analysisPort}" != "80" ]]; then exit 1; fi
