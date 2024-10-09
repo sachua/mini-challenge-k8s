bf63eef9969747bda1cd4c8dd628456da09004d1
sachua <chuasongann@gmail.com>
2024-10-09 15:23:52 +0800

    update background.sh

--- csit-adv-challenge-2024/background.sh
+++ csit-adv-challenge-2024/background.sh
@@ -2,8 +2,6 @@
 
 ssh node01 systemctl stop kubelet
 
-node01ready=$(kubectl get nodes node01 --no-headers | awk '{print $2}')
-
-while [[ "${node01ready}" != "NotReady" ]]; do sleep 1; done
+while [[ "$(kubectl get nodes node01 --no-headers | awk '{print $2}')" != "NotReady" ]]; do sleep 1; done
 
 touch /tmp/finished
